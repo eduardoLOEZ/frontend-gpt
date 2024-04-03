f175c6ba5424d61ee8cdba956980822ad6b0748e
Eduardo Loeza <eduardoloeza@Eduardos-MacBook-Air.local>
2024-04-03 08:52:22 -0600

    un csmbio de estilo

--- src/components/Typer.jsx
+++ src/components/Typer.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 function Typer() {
@@ -8,13 +9,24 @@ function Typer() {
   });
 
   const h1Style = {
-    fontSize: "4rem", // Tamaño de fuente más pequeño
+    fontSize: "4rem", // Tamaño de fuente por defecto
     fontFamily: "Arial, sans-serif", // Cambio de tipo de letra
+  };
+
+  const h1MobileStyle = {
+    fontSize: "2rem", // Tamaño de fuente más pequeño para dispositivos móviles
+    fontFamily: "Arial, sans-serif", // Igual tipo de letra para dispositivos móviles
   };
 
   return (
     <div>
-      <h1 style={h1Style}>
+      <h1 style={h1Style} className="hidden md:block">
+        {text}
+        <span>
+          <Cursor cursorColor="white" />
+        </span>
+      </h1>
+      <h1 style={h1MobileStyle} className="block md:hidden">
         {text}
         <span>
           <Cursor cursorColor="white" />
